08e233e94843a4a6cd39994d6e418fd478c70fe8
Guillaume Barbier <32106445+Biebar@users.noreply.github.com>
2025-07-22 15:08:36 +0200

    feat: choose problem as cmd line argument

--- main.c
+++ main.c
@@ -1,9 +1,34 @@
 #include "problems/problems.h"
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
+static int64_t (*problems[])() = {
+	problem1,
+	problem2,
+};
+constexpr auto num_problems = sizeof problems / sizeof problems[0];
+
+void usage(char const program[static 1]) {
+	fprintf(stderr, "%s [problem number]\n", program);
+}
+
+int main(int argc, char const *argv[argc+1])
 {
-	int64_t result = problem2();
+	size_t problem = num_problems - 1;
+	if(argc == 2) {
+		auto parsed = atoll(argv[1]);
+		if(parsed > 0 && parsed <= num_problems)
+			problem = parsed - 1;
+		else {
+			usage(argv[0]);
+			return 1;
+		}
+	}
+	else if(argc > 1) {
+		usage(argv[0]);
+		return 1;
+	}
+	int64_t result = problems[problem]();
 	printf("%lld\n", result);
 
 	return 0;
