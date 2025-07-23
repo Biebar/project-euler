93feeb515db0c9f9fafaaf672d7fe5d60d0c7312
Guillaume Barbier <32106445+Biebar@users.noreply.github.com>
2025-07-23 16:34:02 +0200

    feat: add problem 10 solution

---
+++ problems/10_summation_of_primes.c
@@ -0,0 +1,13 @@
+#include "../utils/factors.h"
+#include <stdint.h>
+
+int64_t problem10_summation_of_primes()
+{
+	constexpr int64_t input = 2000000;
+	int64_t sum = 0;
+	for (int64_t i = 2; i < input; ++i) {
+		if (is_prime(i))
+			sum += i;
+	}
+	return sum;
+}
