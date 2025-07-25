bcde6e41b2f5046dfc51ae6785abbc013513f3f9
Guillaume Barbier <32106445+Biebar@users.noreply.github.com>
2025-07-25 11:42:21 +0200

    feat: make min and max generic for size and int64

--- utils/unequality.c
+++ utils/unequality.c
@@ -1,4 +1,2 @@
+#define UNEQUALITY_IMPL
 #include "unequality.h"
-
-extern inline int64_t min(int64_t a, int64_t b);
-extern inline int64_t max(int64_t a, int64_t b);
--- utils/unequality.h
+++ utils/unequality.h
@@ -1,16 +1,27 @@
 #ifndef EULER_UTILS_UNEQUALITY_H
 #define EULER_UTILS_UNEQUALITY_H
 
+#include <stddef.h>
 #include <stdint.h>
 
-inline int64_t min(int64_t a, int64_t b)
-{
-	return a > b ? b : a;
-}
-inline int64_t max(int64_t a, int64_t b)
-{
-	return a > b ? a : b;
-}
+#ifdef UNEQUALITY_IMPL
+#define MINMAX_EXTERN extern
+#else
+#define MINMAX_EXTERN
+#endif
+#define MINMAXDEF(name, type, operator)                                        \
+	MINMAX_EXTERN inline type name(type a, type b)                              \
+	{                                                                           \
+		return a operator b ? a : b;                                             \
+	}
+MINMAXDEF(imin, int64_t, <)
+MINMAXDEF(imax, int64_t, >)
+MINMAXDEF(smin, size_t, <)
+MINMAXDEF(smax, size_t, >)
+#define min(a, b) _Generic((a), int64_t: imin, size_t: smin)(a, b)
+#define max(a, b) _Generic((a), int64_t: imax, size_t: smax)(a, b)
+#undef MINMAXDEF
+#undef MINMAX_EXTERN
 inline int64_t iabs(int64_t a)
 {
 	return max(a, -a);
